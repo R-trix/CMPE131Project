1248f51787deb230de081e8ce48a9e281e3835db
Brendan Requierme <requiermeb@gmail.com>
2021-11-27 10:21:39 -0800

    Merged git repo and added login functionality

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 from myapp import myobj
 
 myobj.run(debug=True)
+
+"""
+import time
+
+work_timer = 25 * 60
+break_timer = 5 * 60
+
+while True:
+    t = work_timer
+    mins = t // 60
+    secs = t % 60
+    timer = '{:02d}:{:02d}'.
+"""
