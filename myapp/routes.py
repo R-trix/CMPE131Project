7c30717c0ae686d58adeb36f888ae553e3f5aa9f
jazSingh5 <jaskaransingh3065@gmail.com>
2021-12-06 21:55:31 -0800

    added markdown import

--- myapp/routes.py
+++ myapp/routes.py
@@ -7,6 +7,7 @@ from flask import render_template, flash, redirect, request
 from flask_login import login_user, logout_user, login_required, current_user, UserMixin
 import random
 import pdfkit
+from markdown import markdown
 from werkzeug.utils import secure_filename
 
 @myobj.route("/")
